3a4821aa580c444a5dc7508038ac5d4ac592f7e5
harnoor <harnoor@WILASSETA0036.local>
2025-02-17 18:07:23 +0530

    routes start

--- src/index.js
+++ src/index.js
@@ -17,26 +17,5 @@ connectDB()
     console.log("MongoDB connection failed", err)
 })
 
-/*
-import express from "express"
-const app = express()
-//IIFE
-( async () => {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        app.on("error", (error) => {
-            console.log("ERR: ", error)
-            throw error
-        })
 
-        app.listen(process.env.PORT, () => {
-            console.log(`App is listening on port ${process.env.PORT}`)
-        })
-
-    } catch (error) {
-        console.error("Error:", error)
-        throw err
-    }
-})()
-*/
 
